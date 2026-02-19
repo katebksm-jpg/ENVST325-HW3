4d5bed758c51b55730637584ff284d9da4be5edb
katebksm-jpg <katebksm@gmail.com>
2026-02-19 15:28:55 +0000

    working on HW problems

--- HW3.R
+++ HW3.R
@@ -108,10 +108,47 @@ ggplot(data=Allemissions, aes(x=Entity, y=total_emissions, fill=Entity))+
 
 
 # Homework Prompts --------------------------------------------------------
+#question 1 - 
+unique(datCO2$Entity)
+#create new data set 
+LargestPop <- datCO2[datCO2$Year>=1990 & datCO2$Entity=="India"|
+                       datCO2$Year>=1990 & datCO2$Entity=="China"|
+                       datCO2$Year>=1990 & datCO2$Entity =="United States"|
+                       datCO2$Year>=1990 & datCO2$Entity=="Russia",]
+
+
+ggplot(data = LargestPop, aes(x=Year, y=CO2, color=Entity))+
+  geom_point()+
+  geom_line()+
+  labs(x="Year", y="Annual Emissions (tons CO2)") +
+  theme_classic()
 
 
 
-  
+#question2 - communicating change in world temperature and emissions
+
+#world emissions - create new data set
+
+WorldCO2 <- datCO2[datCO2$Year>=1800 & datCO2$Entity=="World",]
+
+ggplot(data=WorldCO2, aes(x=Year, y=CO2, color=Entity))+
+         geom_line()+
+         geom_point()+
+         labs(x="Year", y="Global Emissions (tons of CO2)")+
+         theme_classic()
+
+#graph global temperature anomaly  
+unique(tempanom$Entity)
+
+WorldTemp <- tempanom[tempanom$Entity=="World",]
+
+ggplot(dat=WorldTemp, aes(x=date, y=temperature_anomaly))+
+  geom_line()+
+  geom_point()+ 
+  geom_hline(yintercept = 0, linetype="dashed", linewidth=1, color="grey")+
+  labs(x="Year", y="Temperature Anomaly(Degrees C)")+
+  theme_classic()+
+  scale_color_manual(values = c("#FF8242",)
 
 
 
